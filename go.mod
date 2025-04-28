54a72d2a5d78bb7297686945753c5590f78f6f9d
Brendan Devenney <brendan@devenney.io>
2025-04-28 23:38:10 +0100

    feat: cors
    Signed-off-by: Brendan Devenney <brendan@devenney.io>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/devenney/tracksmith-backend
 
 go 1.24.2
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/cors v1.2.1
+)
--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	// CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+	}))
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
